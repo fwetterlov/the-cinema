5378934933a8a5cbaa870dcb3290b528766e5bd6
fwett <fwetterlov@gmail.com>
2023-03-15 19:10:02 +0100

    Started on filtering

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col } from 'react-bootstrap';
 
 export default function Movies() {
   const [movies, setMovies] = useState([]);
   const [screenings, setScreenings] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
 
   useEffect(() => {
     (async () => {
@@ -14,6 +15,23 @@ export default function Movies() {
       setScreenings(await screeningsResponse.json());
     })();
   }, []);
+
+  useEffect(() => {
+    const allCategories = movies.reduce((categories, movie) => {
+      movie.description.categories.forEach(category => {
+        if (!categories.includes(category)) {
+          categories.push(category);
+        }
+      });
+      return categories;
+    }, []);
+
+    setCategories(allCategories);
+  }, [movies]);
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
 
   const formatDate = (date) => {
     return new Date(date).toLocaleDateString('en-EN', {
@@ -35,6 +53,12 @@ export default function Movies() {
 
   return (
     <div>
+      <select value={selectedCategory} onChange={handleCategoryChange}>
+        <option value="">All Categories</option>
+        {categories.map(category => (
+          <option key={category} value={category}>{category}</option>
+        ))}
+      </select>
       {Object.keys(groupedScreenings).map((date) => (
         <div key={date} className="date">
           <h2>{date}</h2>
@@ -42,13 +66,13 @@ export default function Movies() {
             {groupedScreenings[date].map(({ id, time, movieId, auditoriumId }) => {
               const movie = movies.find(movie => movie.id === movieId);
               return (
-                <div key={id} className="movie-card">
+                <div key={id} className="movie-card" onClick={() => console.log(movie, date)}>
                   <img src={`https://cinema-rest.nodehill.se${movie.description.posterImage}`} alt={movie.title} />
                   <div className="movie-info">
                     <h3>{movie.title}</h3>
-                    <p>Movie Length: {`${Math.floor(movie.description.length / 60)}h ${movie.description.length % 60}min`}</p>
-                    <p>Time: {new Date(time).toLocaleTimeString('sv-SV')}</p>
-                    <p>Auditorium: {auditoriumId}</p>
+                    <p>{`${Math.floor(movie.description.length / 60)}h ${movie.description.length % 60}min`}</p>
+                    <p>{new Date(time).toLocaleTimeString('sv-SV').substring(0, 5)}</p>
+                    <p>Auditorium {auditoriumId}</p>
                   </div>
                 </div>
               );
