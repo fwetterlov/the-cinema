d4e69aba99b9ba0e170b92977348a10346566faa
fwett <fwetterlov@gmail.com>
2023-03-15 12:58:56 +0100

    navbar completed using React Router

---
+++ src/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        <img src="./img/logo.png" className="site-logo" alt="Logo" />
+        Feature Flicks
+      </Link>
+      <ul>
+        <CustomLink to="/movies">Movies</CustomLink>
+        <CustomLink to="/contact">Contact</CustomLink>
+      </ul>
+    </nav>
+  )
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
+}
