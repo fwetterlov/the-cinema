5f229dacf03f3ed711ad45f038334b54b51330c5
fwett <fwetterlov@gmail.com>
2023-03-24 11:24:03 +0100

    Created mock for startpage and contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,16 @@
 export default function Contact() {
-  return <h1>Contact</h1>
+  return (
+    <div class="contact">
+      <h1>Contact Feature Flicks</h1>
+      <p>Feel free to contact us with any questions or concerns.</p>
+      <h2>Address:</h2>
+      <p>123 Main St, Anytown USA</p>
+      <h2>Email:</h2>
+      <p>contact@featureflicks.com</p>
+      <h2>Phone:</h2>
+      <p>555-1234</p>
+      <h2>Hours:</h2>
+      <p>Monday-Friday: 9am-5pm</p>
+    </div>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1>Welcome to Feature films</h1>
+  return (
+    <div class="start-page">
+      <h1>Welcome to Feature Films</h1>
+      <p>Discover the magic of the theatre!</p>
+      <p>At Feature Films, we bring you the best movies and TV shows from around the world. Our collection includes the latest blockbusters, timeless classics, and everything in between.</p>
+      <p>Whether you're a fan of action, romance, comedy, or drama, we have something for everyone. So sit back, relax, and enjoy the show!</p>
+    </div>
+  );
 }
